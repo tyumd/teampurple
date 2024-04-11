650d365f5b3661d3ca2c5cb4345cd30d770542eb
Ty <thood1@terpmail.umd.edu>
2024-04-11 19:50:15 -0400

    search + loop

--- toll/final.py
+++ toll/final.py
@@ -1,6 +1,7 @@
 class Toll: 
-    def __init__(self, path): 
+    def __init__(self, path, name): 
         self.path = path
+        self.name = name
 
     def search(self, name):
         with open(self.path, 'r') as file:
@@ -8,24 +9,26 @@ class Toll:
                 tollinfo = line.strip().split(",")
                 person = [info.strip() for info in tollinfo]
             
-            if name in person:
-                debtor = {
-                    "name": person[0],
-                    "plate": person[1],
-                    "axle": person[2],
-                    #do we want times_crossed? 
-                    "times_crossed": person[3]
-                }
-                return debtor
+                if name in person:
+                    debtor = {
+                        "name": person[0],
+                        "plate": person[1],
+                        "axle": person[2],
+                        #do we want times_crossed? 
+                        "times_crossed": person[3]
+                    }
+                    return debtor
             
-            if name not in tollinfo:
-                print(f"{name} wasn't found in our record book.")
+            print(f"{name} wasn't found in our record book.")
+            return None
 
 path = "people.txt"
-toll = Toll(path)
 while True: 
-    name = input("Enter a name or enter nothing to quit: ")
-    if name == "":
+    toll = Toll(path, "")
+    name = input("Enter name or blank input to exit: ")
+    if name == "": 
         break
+    toll.name = name
     debtor = toll.search(name)
-    print(debtor['axle'])
+    if debtor:
+        print(debtor.get("axle"))
