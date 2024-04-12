7d8916c54b94219d3d8c1401b4940f2e77d99388
jtoto346 <158086097+jtoto346@users.noreply.github.com>
2024-04-11 20:24:27 -0400

    joe Update final.py

--- toll/final.py
+++ toll/final.py
@@ -32,3 +32,25 @@ while True:
     debtor = toll.search(name)
     if debtor:
         print(debtor.get("axle"))
+        
+def toll(self, name, license_number = None):
+        file_path = "toll/people.txt"
+        self.name = name
+        self.license_number = license_number if license_number is not None \
+                                                                else None
+        with open(file_path, "r", encoding="utf-8" ) as f:
+            for line in f:
+                lines = line.strip().split(",")
+                on_f_name = lines[0].strip()
+                on_f_license_number = lines[1].strip()
+                toll_number = int(lines[2].strip())
+                toll_amount = int(lines[3].strip())
+                balance = toll_number*toll_amount
+
+                if name == on_f_name:
+                    print(f"Hello {on_f_name}, you have a balance of {balance}")
+
+                elif license_number == on_f_license_number:
+                    print(f"Hello {on_f_name}, you have a balance of {balance}")
+                else:
+                    print("We have no record of your transactions")
