6affd4bc94e380e50399e3002094a3a42427a342
tyumd <166080496+tyumd@users.noreply.github.com>
2024-05-06 11:33:38 -0400

    fix

--- toll/final.py
+++ toll/final.py
@@ -41,35 +41,18 @@ class Member:
             total_balance = 0
             if member_info["member"].upper() == "Y":
                 total_balance = I95_a + BHT_a + FMT_a + CBB_a
+                return total_balance
             elif member_info["member"].upper() == "N":
                 total_balance = (I95_a + BHT_a + FMT_a + CBB_a) * 1.5
-            return total_balance
-            
-            print(f"Hello {member_info['name']},\nYou have passed through I95 {member_info['i95_a']} times,\n"
+                return total_balance
+            print(f"Hello {member_info['name']},\nyou have passed through I95 {member_info['i95_a']} times,\n"
                   f"The Fort McHenry Tunnel {member_info['fmt_a']} times,\n"
                   f"The Chesapeake Bay Bridge {member_info['cbb_a']} times,\n"
                   #decimal places and $
-                  f"And your total balance is ${total_balance:.2f}")
+                  f"and your total balance is ${total_balance:.2f}")
             
         else:
             print("Name not found in the records.")
-        
-            
-    def make_payment(self, name):
-        #joe
-        name = input("what is your name? ")
-        member_info = self.get_member(name)
-        payment_balance = self.get_balance(member_info)
-        print(f"Hello {name}, your balance is {payment_balance}.")
-        choice = input("Would you like to pay you balance?(Y/N) ")
-        if choice == "Y":
-            payment_amount = input("How much do you want to pay? ")
-            new_balance = payment_balance - payment_amount
-            print(f"Your updated balance is {new_balance}")
-        elif choice == "N":
-            pass
-        return new_balance 
-        
 
 #fix reading syntax line.strip didnt have () 
 def read_file(filepath):
@@ -81,12 +64,6 @@ def read_file(filepath):
             information = line.strip().split(",")
             data[information[0]] = information
     return data
-
-def new_file(dict, output_file):
-    
-    with open (output_file, "w", encoding = 'utf-8') as outfile:
-        outfile.write(dict)
-    
 
 #graphs some info from txt file. PEOPLE TXT NOT THE NEW ONE
 def get_graph(filepath, choice):
@@ -126,24 +103,19 @@ def main(filepath):
     #judi
     data = read_file(filepath)
     newMem = Member(data)
-    option = input("What would you like to do?:\n1: Search a name\n2: Check average toll fees\n 0: cancel\n")
+    option = input("What would you like to do?:\n1: Search a name\n2: Check average toll fees \n0: cancel\n")
 
     if option == "1":
         name = input("What name do you want to look up?: ")
         member = newMem.get_member(name)
         if member:
             balance = newMem.get_balance(name)
+            print(balance)
         else:
             print(f"{name} not found")
-        response= input("Would you like to make a payment? Y/N")    
-        if response == "Y":
-            charge= newMem.make_payment(name)
-            print(charge)
-        
     elif option == "2":
         choice = input("What do you want to graph?\n1: Members\n2: Vehicle Type\n3: Bridges\n")
         get_graph(filepath, choice)
-    
     elif option == "0":
         print("Terminating")
     else:
